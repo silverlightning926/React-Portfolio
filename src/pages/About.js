7eaf44229508c8eba8ae19f7f064789c400afb6d
Siddharth Rao <siddrao.926@gmail.com>
2020-05-20 02:27:19 -0700

    Changed About Page Bio

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,36 +14,14 @@ class About extends Component
                 <Header title="About"/>
                 <div className="page-body">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nisl risus. In hac habitasse platea dictumst.
-                        Integer eleifend vestibulum lectus et efficitur. Donec a ligula metus. Ut sed tristique lectus, quis aliquet est.
-                        Nullam ante sapien, maximus ac ullamcorper id, vestibulum eget odio. Vivamus odio felis, malesuada et porttitor eu, maximus nec mi.
-                        Fusce vitae pellentesque risus. Proin pellentesque bibendum arcu et vulputate.
-                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-                        Nullam et est a elit tristique placerat.
+                        My name is Siddharth Rao. I am from Seattle Washington. I have been coding ever since I was a kid and am always learning new things.
+                        I am currently used the resources around me to make myself proficient in many things such as Java, Javascript & more.
+                        I have currently made my own software brand named Phoenix Interactive have developed many apps under the brand.
+                        I also have worked with other companies and organizations in the past and have developed apps and applications for them under their name.
                     </p>
 
                     <p>
-                        Proin dapibus scelerisque purus, id facilisis erat laoreet nec.
-                        Morbi sit amet tortor iaculis, pellentesque purus ac, porttitor nunc.
-                        Vestibulum condimentum, tellus vestibulum facilisis sagittis, erat nunc vehicula lacus, consectetur molestie mi enim in sem.
-                        Vivamus arcu nulla, dapibus eu augue ut, euismod feugiat lacus. Nunc dictum consequat nibh, et semper libero pretium quis.
-                        Vivamus mauris sem, gravida id nunc et, commodo hendrerit nisl. Nulla aliquam pharetra sagittis.
-                        Vestibulum tempor pulvinar nibh, in sollicitudin libero hendrerit vitae. Suspendisse pharetra elementum massa in ultrices.
-                        Aenean tempor molestie magna, vel ullamcorper turpis fringilla vitae. Maecenas pretium odio at mattis congue.
-                        Nunc lacus quam, elementum eu massa nec, placerat scelerisque libero. Nulla ac dignissim quam, nec fermentum nunc.
-                        Nullam semper pretium consequat. Aenean iaculis nisi nec dui posuere consequat.
-                        Morbi vulputate ex sit amet tellus posuere feugiat.
-                    </p>
-
-                    <p>
-                        Sed vel pulvinar ipsum, sed blandit enim.
-                        Praesent tincidunt vehicula ligula quis imperdiet.
-                        Nulla nec enim id velit condimentum lobortis. Fusce faucibus odio non diam tincidunt aliquam.
-                        Vivamus vitae dapibus nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                        Aliquam placerat, sapien vel imperdiet imperdiet, mi mauris placerat enim, ut placerat eros turpis in arcu.
-                        Nulla ut metus eu diam molestie sagittis aliquet ac elit. Sed et leo maximus, volutpat lacus at, elementum tellus.
-                        Donec luctus placerat congue. Maecenas vitae lacinia eros. Phasellus maximus erat nibh, nec ullamcorper nisl convallis at.
-                        Phasellus purus ex, scelerisque nec congue a, consectetur ac magna.
+                        If you have an offer or any freelancing work, please feel free to contact me by the Contact page or any of the other methods listed on the Contact Page.
                     </p>
 
                     <h1>Links</h1>
