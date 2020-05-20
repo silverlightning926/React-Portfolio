9cba0469a2c53768d2a0e4942bd583eeb7631d41
Siddharth Rao <siddrao.926@gmail.com>
2020-05-19 23:22:04 -0700

    Changed Size Of Skill Panel

--- src/components/SkillsPanel.js
+++ src/components/SkillsPanel.js
@@ -5,7 +5,7 @@ const SkillsPanel = ({images}) => {
         <div className="center-image">
             {
                 images.map((img, key) => (
-                    <img style={{padding: "0px 20px", margin: "20px 0px",}} key={key} height="125px" src={require(`../assets/images/logoImages/${img}.svg`)} alt = ""/>
+                    <img style={{padding: "0px 20px", margin: "20px 0px",}} key={key} height="85px" src={require(`../assets/images/logoImages/${img}.svg`)} alt = ""/>
                 ))
             }
         </div>
